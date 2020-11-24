bb68986e83034aceeef8e8bfb6b0806ba4ceebc4
ramesh1990 <58243057+ramesh1990@users.noreply.github.com>
2020-11-24 08:10:55 +0530

    Create LongestPalindrome.py

---
+++ Easy/LongestPalindrome.py
@@ -0,0 +1,57 @@
+'''
+Longest Palindrome in a String 
+Medium Accuracy: 31.67% Submissions: 100k+ Points: 4
+Given a string S, find the longest palindromic substring in S. Substring of string S: S[ i . . . . j ] where 0 ≤ i ≤ j < len(S). Palindrome string: A string which reads the same backwards. More formally, S is palindrome if reverse(S) = S. Incase of conflict, return the substring which occurs first ( with the least starting index ).
+
+NOTE: Required Time Complexity O(n2).
+
+Input:
+The first line of input consists number of the testcases. The following T lines consist of a string each.
+
+Output:
+In each separate line print the longest palindrome of the string given in the respective test case.
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ Str Length ≤ 104
+
+Example:
+Input:
+1
+aaaabbaa
+
+Output:
+aabbaa
+'''
+
+
+def longestPalindrome(string):
+    start, end = 0,0
+    n = len(string)
+    if n < 1: 
+        return ""
+        
+    for i in range(n-1):
+        ret1 = isPalindrome(string,i,i)
+        ret2 = isPalindrome(string,i,i+1)
+        lenght = max(ret1,ret2)
+        if lenght > (end-start) :
+            start = i - ((lenght-1)//2)
+            end = i + (lenght//2)
+            
+    return string[start:end+1]
+    
+def isPalindrome(string,left,right):
+    print (left,right)
+    if left > right :
+        return 0
+    while ( left >= 0 and right < len(string) and ( string[left] == string[right] )) :
+        left -= 1
+        right += 1
+        
+    return right - left - 1
+    
+    
+    
+s = 'aaaabbaa'
+print ("\n Longest Palindrome in a String : ",longestPalindrome(s))
